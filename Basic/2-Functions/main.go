ed156e10ee544bb6b757369eea558553a64b677f
fepc18 <fepc18@gmail.com>
2023-07-02 09:36:37 -0500

    3-Packages, example with package math

--- Basic/2-Functions/main.go
+++ Basic/2-Functions/main.go
@@ -17,8 +17,7 @@ func plusPlus(a, b, c int) int {
 // Function returning a value with named return value
 
 func rectangle(l int, b int) (area int) {
-	var parameter int
-	parameter = 2 * (l + b)
+	parameter := 2 * (l + b)
 	fmt.Println("Parameter: ", parameter)
 
 	area = l * b
@@ -29,6 +28,15 @@ func rectangle(l int, b int) (area int) {
 func swap(x, y string) (string, string) {
 	return y, x
 }
+
+// Passing Address to a Function
+func update(a *int, t *string) {
+	*a = *a + 5      // defrencing pointer address, donde esta el valor
+	*t = *t + " Doe" // defrencing pointer address
+}
+
+//Anonymous Functions in Golang
+// Anonymous functions are the functions that don’t have any name.
 
 func main() {
 	// Function accepting arguments
@@ -43,5 +51,20 @@ func main() {
 	// Function returning multiple values
 	a, b := swap("Mahesh", "Kumar")
 	fmt.Println(a, b)
+
+	// Passing Address to a Function
+	var age = 20
+	var text = "John"
+	fmt.Println("Before:", text, age)
+	update(&age, &text) // Passing address to a function
+	fmt.Println("After :", text, age)
+
+	//Anonymous Functions in Golang
+	fmt.Printf(
+		"100 (°F) = %.2f (°C)\n",
+		func(f float64) float64 {
+			return (f - 32.0) * (5.0 / 9.0)
+		}(100),
+	)
 
 }
