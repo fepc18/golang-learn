9d632fffeb8ff0b7f5c12c9e592e827124984c4e
fepc18 <fepc18@gmail.com>
2023-07-03 19:46:03 -0500

    add switch types example

--- Deeper/1-TypeAssertions/main.go
+++ Deeper/1-TypeAssertions/main.go
@@ -14,6 +14,28 @@ func main() {
 	f, ok := i.(float64)
 	fmt.Println(f, ok)
 
-	f = i.(float64) // panic
-	fmt.Println(f)
+	//f = i.(float64) // panic
+	//fmt.Println(f)
+
+	/// Type Switch
+	// A type switch is a construct that permits several type assertions in series.
+	var t interface{}
+	t = functionOfSomeType()
+	switch t := t.(type) {
+	default:
+		fmt.Printf("unexpected type %T\n", t) // %T prints whatever type t has
+	case bool:
+		fmt.Printf("boolean %t\n", t) // t has type bool
+	case int:
+		fmt.Printf("integer %d\n", t) // t has type int
+	case *bool:
+		fmt.Printf("pointer to boolean %t\n", *t) // t has type *bool
+	case *int:
+		fmt.Printf("pointer to integer %d\n", *t) // t has type *int
+	}
+
+}
+
+func functionOfSomeType() interface{} {
+	return 10
 }
